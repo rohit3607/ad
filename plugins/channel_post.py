2a5a42d64202ede125403970546033a3a60bdff1
rohit3607 <rohitplayer87089@gmail.com>
2024-12-08 19:39:14 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -8,6 +8,7 @@ from pyrogram.errors import FloodWait
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
+from database.database import *
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','add_admin','del_admin','admins']))
 async def channel_post(client: Client, message: Message):
