88f10c691ca46e1b9e808fc4a8d9579634e43e12
rohit3607 <rohitplayer87089@gmail.com>
2024-12-09 13:11:22 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -3,21 +3,28 @@ from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
-from helper_func import get_readable_time
 from database.database import admins_collection  
 
-db_admins = [admin['id'] for admin in admins_collection.find({}, {"_id": 0, "id": 1})]  
-combined_admins = list(set(ADMINS) | set(db_admins))  
+# Create the bot instance
+bot = Bot()
 
+# Set bot uptime
 bot.uptime = datetime.now()
 
+# Combine admin lists
+db_admins = [admin['id'] for admin in admins_collection.find({}, {"_id": 0, "id": 1})]
+combined_admins = list(set(ADMINS) | set(db_admins))
+
+# Helper function to get readable time
 def get_readable_time(seconds):
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{hours}h {minutes}m {seconds}s"
 
+# Bot stats text
 BOT_STATS_TEXT = "Bot uptime: {uptime}"
 
+# Command to show stats
 @Bot.on_message(filters.command('stats') & filters.user(combined_admins))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
@@ -25,13 +32,15 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
+# Respond to private messages
 @Bot.on_message(filters.private)
 async def useless(_, message: Message):
     user_id = message.from_user.id
 
-    # Check if the user is in the combined admin list
+    # Skip responding to admins
     if user_id in combined_admins:
-        return  # Do not send USER_REPLY_TEXT to admins
+        return  
 
+    # Reply with USER_REPLY_TEXT if defined
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
